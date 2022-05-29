0b269d471d42a2023f86f99230d9365aaafb2265
Taha Abdelrahman <75391474+taha-abdelrahman@users.noreply.github.com>
2022-05-29 11:55:24 +0200

    Delete database.js

--- database.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-let Data = new mongoose.Schema({
-guild: String,
-channel: String,
-author: String,
-count: Number
-});
-
-module.exports = mongoose.model("counts-data", Data);
